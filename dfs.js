26c61f58eb2047b590b3bec2c2c74dac1b3cf070
melvilgit <melvil.mec@gmail.com>
2015-01-01 23:07:44 +0530

    Update dfs.js

--- dfs.js
+++ dfs.js
@@ -1,22 +1,22 @@
 function graphtraversal(vertex) {
-	  var arr = [
-		      [0, 1, 1, 0],
-	      [0, 0, 1, 0],
-	          [1, 0, 0, 1],
-		      [0, 0, 0, 1]
-			        ];
-	    var edges = [0, 1, 2, 3];
-	      var visited = {};
-	        var stack = [];
-		  stack.push(vertex);
-		    while (stack.length > 0) {
-			        var p = stack.pop();
-				    console.log("pop is" + p);
-				        if (!visited[p]) {
-						      visited[p] = true;
-						            var nearbyedges = findnearby(p, arr, visited);
-							          stack = stack.concat(nearbyedges);
-								      }
-					  }
-		      return visited;
+  var arr = [
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [1, 0, 0, 1],
+    [0, 0, 0, 1]
+  ];
+  var edges = [0, 1, 2, 3];
+  var visited = {};
+  var stack = [];
+  stack.push(vertex);
+  while (stack.length > 0) {
+    var p = stack.pop();
+    console.log("pop is" + p);
+    if (!visited[p]) {
+      visited[p] = true;
+      var nearbyedges = findnearby(p, arr, visited);
+      stack = stack.concat(nearbyedges);
+    }
+  }
+  return visited;
 }
